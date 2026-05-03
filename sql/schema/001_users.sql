b2dc2fe9ffcac9b92fcaff7c9e37282dfb3d7093
kaiserkimguin <kaiserkimguin>
2026-05-03 18:45:34 +0200

    sql/schema added, first migration completed

---
+++ sql/schema/001_users.sql
@@ -0,0 +1,11 @@
+-- +goose Up
+CREATE TABLE users ( 
+  id INTEGER PRIMARY KEY,
+  created_at TIMESTAMP NOT NULL,
+  updated_at TIMESTAMP NOT NULL,
+  name VARCHAR UNIQUE NOT NULL
+);
+
+-- +goose Down
+DROP TABLE users;
+
